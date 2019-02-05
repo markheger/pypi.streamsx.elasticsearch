12a2af87952d51165edc7b6a1e1593f67057eee7
markheger <mark-oliver.heger@de.ibm.com>
2019-02-05 10:13:53 +0100

    1.0

--- README.md
+++ README.md
@@ -1,2 +1,41 @@
-# pypi.streamsx.elasticsearch
-[INCUBATION] This is a step in allowing natural use of Streams for a Python developer. A project that will be registered with PyPi to allow 'pip install' of Python packages that support Python developers interacting with IBM Streams.
+# Python streamsx.elasticsearch package
+
+This exposes SPL operators in the `com.ibm.streamsx.elasticsearch` toolkit as Python methods.
+
+Package is organized using standard packaging to upload to PyPi.
+
+The package is uploaded to PyPi in the standard way:
+```
+cd package
+python setup.py sdist bdist_wheel upload -r pypi
+```
+Note: This is done using the `ibmstreams` account at pypi.org and requires `.pypirc` file containing the credentials in your home directory.
+
+Package details: https://pypi.python.org/pypi/streamsx.elasticsearch
+
+Documentation is using Sphinx and can be built locally using:
+```
+cd package/docs
+make html
+```
+and viewed using
+```
+firefox package/docs/build/html/index.html
+```
+
+The documentation is also setup at `readthedocs.io`.
+
+Documentation links:
+* http://streamsxelasticsearch.readthedocs.io
+
+## Test
+
+Package can be tested with TopologyTester using the [Streaming Analytics](https://www.ibm.com/cloud/streaming-analytics) service and [Compose for Elasticsearch](https://www.ibm.com/cloud/compose/elasticsearch) service on IBM Cloud.
+
+Store connection parameters in application configuration, default name is 'es', or set the environment variable `ELASTICSEARCH_CONNECTION` with the connection string of the Elasticsearch cloud service.
+
+```
+export ELASTICSEARCH_TOOLKIT_HOME=<toolkit_location>
+cd package
+python3 -u -m unittest streamsx.elasticsearch.tests.test_elasticsearch.TestES
+```
---
+++ package/streamsx/elasticsearch/tests/test_elasticsearch.py
@@ -0,0 +1,140 @@
+from unittest import TestCase
+
+from streamsx.topology.topology import Topology
+from streamsx.topology.tester import Tester
+from streamsx.topology.schema import CommonSchema, StreamSchema
+import streamsx.elasticsearch as es
+import streamsx.spl.toolkit
+import streamsx.rest as sr
+import os
+import uuid
+
+##
+## Test assumptions
+##
+## Streaming analytics service has:
+##    application config 'es' configured for Elasticsearch database
+## or
+## ELASTICSEARCH_CONNECTION environment variable is set with Elasticsearch cloud service connection string
+##
+
+class JsonData(object):
+    def __init__(self, prefix, count):
+        self.prefix = prefix
+        self.count = count
+    def __call__(self):
+        for i in range(self.count):
+            yield {'p': self.prefix + '_' + str(i), 'c': i}
+
+class StringData(object):
+    def __init__(self, prefix, count):
+        self.prefix = prefix
+        self.count = count
+    def __call__(self):
+        for i in range(self.count):
+            yield self.prefix + '_' + str(i)
+        
+def get_credentials():
+    result = 'es'
+    try:
+        result = os.environ['ELASTICSEARCH_CONNECTION']
+    except KeyError: 
+        result = 'es'
+    return result
+
+
+class TestES(TestCase):
+    def setUp(self):
+        Tester.setup_distributed(self)
+        self.es_toolkit_home = os.environ["ELASTICSEARCH_TOOLKIT_HOME"]
+
+    def test_json(self):
+        n = 100
+        topo = Topology()
+        streamsx.spl.toolkit.add_toolkit(topo, self.es_toolkit_home)
+
+        uid = str(uuid.uuid4())
+        s = topo.source(JsonData(uid, n)).as_json()
+        es.bulk_insert(s, 'test-index-cloud', 10, credentials=get_credentials())
+
+        tester = Tester(topo)
+        tester.run_for(60)
+        # setup test config
+        cfg = {}
+        job_config = streamsx.topology.context.JobConfig(tracing='info')
+        job_config.add(cfg)
+        cfg[streamsx.topology.context.ConfigParams.SSL_VERIFY] = False     
+        # Run the test
+        tester.test(self.test_ctxtype, cfg, always_collect_logs=True)
+
+    def test_string(self):
+        n = 100
+        topo = Topology()
+        streamsx.spl.toolkit.add_toolkit(topo, self.es_toolkit_home)
+
+        uid = str(uuid.uuid4())
+        s = topo.source(StringData(uid, n)).as_string()
+        es.bulk_insert(s, 'test-index-cloud', 10, credentials=get_credentials())
+
+        tester = Tester(topo)
+        tester.run_for(60)
+        # setup test config
+        cfg = {}
+        job_config = streamsx.topology.context.JobConfig(tracing='info')
+        job_config.add(cfg)
+        cfg[streamsx.topology.context.ConfigParams.SSL_VERIFY] = False     
+        # Run the test
+        tester.test(self.test_ctxtype, cfg, always_collect_logs=True)
+ 
+    def test_hw(self):
+        n = 100
+        topo = Topology()
+        streamsx.spl.toolkit.add_toolkit(topo, self.es_toolkit_home)
+
+        s = topo.source(['Hello', 'World!']).as_string()
+        es.bulk_insert(s, 'test-index-cloud', credentials=get_credentials())
+
+        tester = Tester(topo)
+        tester.run_for(60)
+        # setup test config
+        cfg = {}
+        job_config = streamsx.topology.context.JobConfig(tracing='info')
+        job_config.add(cfg)
+        cfg[streamsx.topology.context.ConfigParams.SSL_VERIFY] = False     
+        # Run the test
+        tester.test(self.test_ctxtype, cfg, always_collect_logs=True)
+
+    def test_schema(self):
+        schema = StreamSchema('tuple<rstring indexName, rstring document>')
+        topo = Topology()
+        streamsx.spl.toolkit.add_toolkit(topo, self.es_toolkit_home)
+
+        s = topo.source([('idx1','{"msg":"This is message number 1"}'), ('idx2','{"msg":"This is message number 2"}')])
+        s = s.map(lambda x : x, schema=schema)
+        s.print()
+        es.bulk_insert_dynamic(s, index_name_attribute='indexName', message_attribute='document', credentials=get_credentials())
+
+        tester = Tester(topo)
+        tester.run_for(60)
+
+        # setup test config
+        cfg = {}
+        job_config = streamsx.topology.context.JobConfig(tracing='info')
+        job_config.add(cfg)
+        cfg[streamsx.topology.context.ConfigParams.SSL_VERIFY] = False     
+        # Run the test
+        tester.test(self.test_ctxtype, cfg, always_collect_logs=True)
+
+class TestCloud(TestES):
+    def setUp(self):
+        self.es_toolkit_home = os.environ["ELASTICSEARCH_TOOLKIT_HOME"]
+        Tester.setup_streaming_analytics(self, force_remote_build=True)
+
+    @classmethod
+    def setUpClass(self):
+        # start streams service
+        connection = sr.StreamingAnalyticsConnection()
+        service = connection.get_streaming_analytics()
+        result = service.start_instance()
+
+
