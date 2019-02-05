39bc5ee256df3f939340c2c18f2f23823d5acba1
markheger <mark-oliver.heger@de.ibm.com>
2019-02-05 10:31:24 +0100

    update development status

--- package/setup.py
+++ package/setup.py
@@ -13,7 +13,7 @@ setup(
   url = 'https://github.com/IBMStreams/streamsx.elasticsearch',
   keywords = ['streams', 'ibmstreams', 'streaming', 'analytics', 'streaming-analytics', 'elasticsearch'],
   classifiers = [
-    'Development Status :: 3 - Alpha',
+    'Development Status :: 5 - Production/Stable',
     'License :: OSI Approved :: Apache Software License',
     'Programming Language :: Python :: 3',
     'Programming Language :: Python :: 3.5',
